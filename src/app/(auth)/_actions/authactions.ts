e91e3809d7800b66de7a0397dec3939fc3d9627d
Muntasir <alimuntasir2001@gmail.com>
2026-07-30 15:04:23 +0600

    cookie added+redirection

--- src/app/(auth)/_actions/authactions.ts
+++ src/app/(auth)/_actions/authactions.ts
@@ -1,44 +1,59 @@
-"use server"
+"use server";
 
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 type LoginState = {
-    success: true,
-    "message": "Login successful",
-    statuscode: number,
-    data:{
-        accessToken: string,
-        refreshToken: string,
-    }
-    }
-
+    success: boolean;
+    message: string;
+    statuscode?: number;
+    data: {
+        user: any;
+        accessToken: string;
+        refreshToken?: string; 
+    };
+};
 
 export const loginAction = async (prevState: LoginState, formData: FormData) => {
-console.log("Form Data Received:", formData);
+    console.log("Form Data Received:", formData);
 
-const email = formData.get("email") as string;
-const password = formData.get("password") as string;
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
 
-console.log("Email:", email);
-console.log("Password:", password);
+    const payload = {
+        email,
+        password,
+    };
 
-const payload = {
-    email,
-    password,
-}
-
-const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/auth/login`, {
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-    },
-    body: JSON.stringify({...payload })});
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/api/auth/login`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ...payload }),
+    });
 
     const result = await response.json();
     console.log("Login Response Result:", result);
 
+    if (result.success) {
+        const cookieStore = await cookies();
+
+        cookieStore.set("accessToken", result.data.accessToken, {
+            httpOnly: true,
+            maxAge: 60 * 60 * 24 * 7,
+            sameSite: "lax",
+        });
+
+        if (result.data.refreshToken) {
+            cookieStore.set("refreshToken", result.data.refreshToken, {
+                httpOnly: true,
+                maxAge: 60 * 60 * 24 * 7,
+                sameSite: "lax",
+            });
+        }
+        redirect("/user-dashboard", "replace");
+    }
+
     return result;
-
-    
-
-
-}
+};
